93e3b808620a0cef5a44722b2b4bd8882348dc95
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-20 19:34:47 -0700

    feat: configure webpack to accept path alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,19 @@
+const path = require("path")
+
 module.exports = {
   stories: ["../src/**/*.stories.jsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async config => {
+    config.resolve.modules = [
+      path.resolve(__dirname, "..", "src"),
+      "node_modules",
+    ]
+
+    // Alternately, for an alias:
+    config.resolve.alias = {
+      "@components": path.resolve(__dirname, "..", "src", "components"),
+    }
+
+    return config
+  },
 }
