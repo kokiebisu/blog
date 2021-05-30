e354607a4d458ebd3ef8455aaf67dfdc2a78a824
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-29 18:47:45 -0700

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,6 @@ module.exports = {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 1035,
-              sizeByPixelDensity: true,
             },
           },
           {
