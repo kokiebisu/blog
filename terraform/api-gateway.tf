2886130d406b2d64d8136ce9e0251e29614054a1
kokiebisu <a01056715@gmail.com>
2021-07-11 13:55:55 -0700

    feat: terraform script correct

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -6,8 +6,8 @@ resource "aws_api_gateway_rest_api" "kocoblo" {
 
 resource "aws_api_gateway_resource" "form" {
     rest_api_id = aws_api_gateway_rest_api.kocoblo.id
-    parent_id = aws_api_gateway_rest_api.api.root_resource_id
-    path_part = "/form"
+    parent_id = aws_api_gateway_rest_api.kocoblo.root_resource_id
+    path_part = "form"
 }
 
 resource "aws_api_gateway_method" "form" {
@@ -20,19 +20,31 @@ resource "aws_api_gateway_method" "form" {
 resource "aws_api_gateway_integration" "form" {
     rest_api_id = aws_api_gateway_rest_api.kocoblo.id
     resource_id = aws_api_gateway_resource.form.id
-    http_method = "POST"
-    type = "AWS_PROXY"
+    http_method = aws_api_gateway_method.form.http_method
+    integration_http_method = "POST"
+    type = "AWS"
     uri = aws_lambda_function.form.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "form" {
+    rest_api_id = aws_api_gateway_rest_api.kocoblo.id
+    resource_id = aws_api_gateway_resource.form.id
+    http_method = aws_api_gateway_method.form.http_method
+    status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "form" {
+    rest_api_id = aws_api_gateway_rest_api.kocoblo.id
+    resource_id = aws_api_gateway_resource.form.id
+    http_method = aws_api_gateway_method.form.http_method
+    status_code = aws_api_gateway_method_response.form.status_code
 }
 
 resource "aws_api_gateway_deployment" "kocoblo" {
     depends_on = [aws_api_gateway_integration.form]
-
     rest_api_id = aws_api_gateway_rest_api.kocoblo.id
     stage_name = "prod"
 }
-
-### Allows API Gateway to access Lambda
 resource "aws_lambda_permission" "lambda_permission" {
     statement_id = "AllowLambdaInvocation"
     action = "lambda:InvokeFunction"
