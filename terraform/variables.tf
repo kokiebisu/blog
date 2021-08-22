d050a4307263c1dc2a41c77c132c6d6cc46de7b5
kokiebisu <a01056715@gmail.com>
2021-08-22 10:13:51 -0700

    fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,13 @@ variable "site_domain" {
     type = string
     default = "kocoblo.com"
 }
+
+variable "AWS_ACCESS_KEY" {
+    type = string
+    sensitive = true
+}
+
+variable "AWS_SECRET_KEY" {
+    type = string
+    sensitive = true
+}
