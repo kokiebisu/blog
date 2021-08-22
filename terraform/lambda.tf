172413711320c635747ac02f0490a9ac314adec9
kokiebisu <a01056715@gmail.com>
2021-08-22 13:25:26 -0700

    fxi

---
+++ terraform/lambda.tf
@@ -0,0 +1,36 @@
+
+resource "aws_lambda_function" "form_lambda" {
+  function_name = "SendGridForm"
+  handler = "index.handler"
+  runtime = "nodejs14.x"
+  image_uri = aws_ecr
+
+  role = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = {
+      SENDGRID_API_KEY = var.SENDGRID_API_KEY
+      SENDGRID_AUTHORIZED_EMAIL = var.SENDGRID_AUTHORIZED_EMAIL
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "kocoblo-lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
