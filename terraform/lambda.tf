5da393547d5605ff51c28f86ec2274072413f276
kokiebisu <a01056715@gmail.com>
2021-08-22 14:36:32 -0700

    fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,5 @@
 
 resource "aws_lambda_function" "form_lambda" {
-  count = data.aws_ecr_image.form_image.id ? 1 : 0
   function_name = "SendGridForm"
   handler = "index.handler"
   runtime = "nodejs14.x"
