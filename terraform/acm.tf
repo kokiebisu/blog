2fb3ffe59c07e4e433ca5d2722dede9b9f56f683
kokiebisu <a01056715@gmail.com>
2021-08-22 17:51:47 -0700

    fix

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -10,5 +10,5 @@ resource "aws_acm_certificate" "certificate" {
 
 resource "aws_acm_certificate_validation" "validation" {
   certificate_arn         = aws_acm_certificate.certificate.arn
-  validation_record_fqdns = [aws_route53_record.validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
