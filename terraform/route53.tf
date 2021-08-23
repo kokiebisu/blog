6a433187cf301f81c3cc77a44e7ed70211399225
kokiebisu <a01056715@gmail.com>
2021-08-22 17:47:22 -0700

    fix

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,7 +3,7 @@ data "aws_route53_zone" "public" {
   private_zone = false
 }
 
-resource "aws_route53_record" "default" {
+resource "aws_route53_record" "validation" {
   for_each = {
     for option in aws_acm_certificate.certificate.domain_validation_options : option.domain_name => {
       name   = option.resource_record_name
@@ -17,7 +17,7 @@ resource "aws_route53_record" "default" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.this.zone_id
+  zone_id         = data.aws_route53_zone.validation.zone_id
 }
 
 resource "aws_route53_record" "plain" {
