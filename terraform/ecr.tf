3677bb70dc969055e57debe123415e9084f1e94e
kokiebisu <a01056715@gmail.com>
2021-08-29 06:26:48 +0900

    publish

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "repository" {
-  name                 = "form"
+  name                 = "kocoblo/form"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
