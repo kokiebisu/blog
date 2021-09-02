c517bac95eabf3444ec21d96da874248fecb1d4d
kokiebisu <a01056715@gmail.com>
2021-09-02 15:02:46 +0900

    fix

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,11 +1,11 @@
-resource "aws_ecr_repository" "repository" {
-  name                 = "kocoblo/form"
-  image_tag_mutability = "MUTABLE"
+# resource "aws_ecr_repository" "repository" {
+#   name                 = "kocoblo/form"
+#   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#   image_scanning_configuration {
+#     scan_on_push = true
+#   }
+# }
 
 # data "aws_ecr_image" "form_image" {
 #     repository_name = aws_ecr_repository.repository.name
