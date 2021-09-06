2e136135a778c4df0335dbabe3a90386d09d2a53
kokiebisu <a01056715@gmail.com>
2021-09-06 09:40:38 +0900

    fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,6 +3,7 @@ resource "aws_lambda_function" "form_lambda" {
   function_name = "SendGridForm"
   handler = "index.handler"
   runtime = "nodejs14.x"
+  package_type = "image"
   image_uri = "${aws_ecr_repository.repository.repository_url}@${data.aws_ecr_image.form_image.id}"
 
   role = aws_iam_role.lambda_role.arn
