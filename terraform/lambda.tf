4d8b7d5d78ebe4a306367490937c8af6d1f49a4e
kokiebisu <a01056715@gmail.com>
2021-09-06 11:21:21 +0900

    fix

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,22 +18,3 @@ resource "aws_lambda_function" "form_lambda" {
   }
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "kocoblo-lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
