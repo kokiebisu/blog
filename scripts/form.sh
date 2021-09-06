2ea369ba1a6e9cd48d2b6be3195d91bc10a30b83
kokiebisu <a01056715@gmail.com>
2021-09-06 15:41:38 +0900

    configure: github actions

--- scripts/form.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 776733965771.dkr.ecr.us-east-1.amazonaws.com
-cd api/form
-docker build -t kocoblo/form .
-docker tag kocoblo/form:latest 776733965771.dkr.ecr.us-east-1.amazonaws.com/kocoblo/form:latest
-docker push 776733965771.dkr.ecr.us-east-1.amazonaws.com/kocoblo/form:latest
